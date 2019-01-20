934cd65b75545e75dbdac8a40e370fec5e84dc70
MoncefBkb <31049591+Princeofgame@users.noreply.github.com>
2019-01-20 15:47:25 +0100

    Ajouté l'API

--- API Manager Php/API.php
+++ API Manager Php/API.php
@@ -18,17 +18,19 @@ class APIManager
             'Access' => $Access
         );
         
+        // On initialise la requête cURL 
         $curl = curl_init($this->url);
         
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postfields));
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
+        
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // Retourner les valeurs dans la fonction curl_exec 
+        curl_setopt($curl, CURLOPT_POST, true);          // Requête = POST
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postfields)); // On envoie le tableau de données à insérer sous forme de JSON
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(  // On spécifie le type de données (JSON) dans le HEADER http                                                                    
             'Content-Type: application/json'                                                                      
         )); 
         
-        $return = curl_exec($curl);
-        curl_close($curl);
+        $return = curl_exec($curl); // On stocke les informations envoyées par l'API dans la variable $return
+        curl_close($curl); // On ferme la connexion
         return $return;
     }
 
@@ -36,7 +38,7 @@ class APIManager
         $curl = curl_init($this->url);
         
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT"); // Requête = PUT 
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postfields));
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
             'Content-Type: application/json'                                                                      
@@ -61,14 +63,13 @@ class APIManager
         $curl = curl_init($this->url. '/' .$id);
         
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE"); // Requête = DELETE
 
         $return = curl_exec($curl);
         curl_close($curl);
         return $return;
     }   
 }
-
 
 $postfields = array(
     'Nom' => 'Boukhecheba',
@@ -77,11 +78,8 @@ $postfields = array(
     'id_visiteur' => 2
 );
 
-
 $Api = new APIManager("http://localhost:3000/visiteurs");
 $res = $Api->put($postfields);
 
 var_dump(json_decode($res));
-
-
 ?>
---
+++ API Node js/API.js
@@ -0,0 +1,79 @@
+ 
+
+/* On importe les bibliothèques essentielles (Express pour l'API, Mysql pour la BDD, 
+et Body-parser pour lire le body des requêtes http)*/
+var express = require('express'); 
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+
+// On spécifie le port et l'url du serveur NodeJs
+var hostname = 'localhost'; 
+var port = 3000; 
+
+// On crée une application Express et un routeur Express
+var app = express(); 
+var myRouter = express.Router();
+
+/* On attache le bodyparser à l'application, 
+cela va permettre à l'application d'utiliser des fonctions permettant de lire les requêtes http facilement (en tant que JSON dans notre cas)*/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// On prépare la connexion Mysql
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "245136",
+	database: "bde_nationale"
+});
+
+// On se connecte à la BDD
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Successfully connected to the database.");
+});
+
+// On specifie quelle URI va enclencher l'API pour les requêtes GET et DELETE
+ myRouter.route('/visiteurs/:id_visiteur')
+ .get(function(req,res){
+ 	con.query("SELECT * FROM visiteurs WHERE id_visiteur = ?", req.params.id_visiteur, function (err, result, fields) {
+ 		if (err) throw err;
+ 		res.send(result);
+ 	});
+ })
+ .delete(function(req,res){
+ 	con.query("DELETE FROM visiteurs WHERE id_visiteur = ?", req.params.id_visiteur, function (err, result, fields) {
+ 		if (err) throw err;
+ 		res.send(result);
+ 	});
+ });
+
+// On specifie quelle URI va enclencher l'API pour les requêtes PUT ET POST
+ myRouter.route('/visiteurs')
+ .post(function(req,res){
+ 	con.query("INSERT INTO visiteurs (Nom, prenom, email, password, id_centre, access) VALUES (?,?,?,?,?,?)", 
+ 		[req.body.Nom, req.body.Prenom, req.body.Email, req.body.Password, req.body.Id_centre, req.body.Access],
+ 		
+ 		function (err, result, fields) {
+ 			if (err) throw err;
+ 				res.send(result);
+ 			});
+ 		})	
+ .put(function(req,res){
+
+ 	con.query("UPDATE visiteurs SET password = ? WHERE id_visiteur = ?", 
+ 		[req.body.Password, req.body.id_visiteur], function (err, result, fields) {
+ 		
+ 		if (err) throw err;
+ 		res.send(result);
+ 		});
+
+ });
+
+// On spécifie à l'application d'utiliser le routeur qu'on vient de créer 
+app.use(myRouter);  
+
+// On lance le serveur EXPRESS
+app.listen(port, hostname, function(){
+ 	console.log("URL http://" + hostname + ":" +port); 
+});
